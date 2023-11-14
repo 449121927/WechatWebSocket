748ae2d43c91d7fcb14d2d1f70534bd709cfeede
Winston <77579759+449121927@users.noreply.github.com>
2023-11-15 00:05:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,10 @@ app.get("/api/wx_openid", async (req, res) => {
 });
 
 const port = process.env.PORT || 80;
-
+async function bootstrap() {
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
 
 bootstrap();
