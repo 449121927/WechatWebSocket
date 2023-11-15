76ffed5bcb2be64339651de8d1f32eeb5356bc74
Winston <77579759+449121927@users.noreply.github.com>
2023-11-15 12:01:38 +0800

    1115_2 Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express();
-const expressWs = require('express-ws');
-expressWs(app);
 
-app.use(express.static("public"));
+
+const app = express();
+const expressWs = require('express-ws')(app);
+const wss = []
 
 app.get('/', function (req, res, next) {
   res.send('Hello World!')
@@ -12,32 +12,23 @@ app.get('/', function (req, res, next) {
 app.ws('/', function (ws, req) {
   console.log('connect success')
   console.log(ws)
-  
+
   // 使用 ws 的 send 方法向连接另一端的客户端发送数据
   ws.send('connect to express server with WebSocket success')
 
-  // 使用 on 方法监听事件
-  //   message 事件表示从另一段（服务端）传入的数据
-  ws.on('message', (data) => {
-    // data: 接收信息
-    server.clients.forEach((item) => {
-      if (item.readyState === ws.OPEN) {
-        // console.log('' + data);
-        item.send('' + data);
-      }
-    });
-  });
-
-  // // 设置定时发送消息
-  // let timer = setInterval(() => {
-  //   ws.send(`interval message ${new Date()}`)
-  // }, 2000)
-
+  wss.push(ws)
+  ws.on("message", (msg) => {
+    console.log(msg);
+    // 给所有的客户端广播消息
+    // console.log(msg);
+    // console.log(wss);
+    wss.forEach((e) => {
+      e.send(msg)
+    })
+  })
   // close 事件表示客户端断开连接时执行的回调函数
   ws.on('close', function (e) {
     console.log('close connection')
-    clearInterval(timer)
-    timer = undefined
   })
 })
 
